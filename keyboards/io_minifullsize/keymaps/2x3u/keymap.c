d02184e303d6d1ec53a958a11f2fd0a8a7ae5f5f
juan <jpuerto96@hotmail.com>
2022-02-21 22:50:13 -0500

    Added 2x3u layout

---
+++ keyboards/io_minifullsize/keymaps/2x3u/keymap.c
@@ -0,0 +1,61 @@
+// Copyright 2022 s8erdude (@jpuerto96)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+#include "analog.h"
+int16_t max_pot_val = 1023;
+int16_t max_ticks = 20;
+int16_t pot_oldVal = 0;
+int16_t pot_val    = 0;
+bool moving     = false;
+#define POT_TOLERANCE 50
+#define POT_PIN D0
+#include "print.h"
+
+
+void matrix_init_user(void) {
+    analogReference(ADC_REF_POWER);
+}
+
+void matrix_scan_user(void){
+    pot_val   = (analogReadPin(POT_PIN));
+
+    // If there is a big enough change, then we need to do something
+    if (abs(pot_val - pot_oldVal) > POT_TOLERANCE) {
+        moving = true;
+        pot_oldVal = pot_val;
+    }
+    else{
+        if (moving){
+            // Do some fancy conversion to get 'absolute' position to num tap_codes to send
+            // Reset moving to 0 so that we don't get multiple attempts to do this
+            int num_ticks = ((float)pot_val/max_pot_val)*max_ticks;
+            for (int i = 0; i<max_ticks;++i){
+                tap_code(KC__VOLDOWN);
+            }
+            for (int i = 0; i<num_ticks;++i){
+                tap_code(KC__VOLUP);
+            }
+            moving = false;
+        }
+    }
+}
+
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+        [_BASE] = LAYOUT_2x3u(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_F13,                            KC_MPLY, KC_VOLU, KC_VOLD, KC_MSTP, 
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_ESC, KC_INS, KC_HOME, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         KC_DEL, KC_END,  KC_P7,   KC_P8,   KC_P9,   KC_NO,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                               KC_UP,       KC_P1,   KC_P2,   KC_P3,   KC_NO,
+        KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,           KC_SPC,           KC_RALT, KC_RGUI, KC_RCTL,                               KC_LEFT, KC_DOWN, KC_RIGHT,    KC_P0,   KC_PDOT, KC_PENT
+    )
+};
